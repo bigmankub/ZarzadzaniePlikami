7d9af4e3f1edad3087ea1d43d1ef85ae21cf05fa
Jakub Dąbrowski <Jakub Dbrowski@ZMIEN-MNIE>
2023-12-11 15:20:58 +0100

    Add project files.

---
+++ ZarzadzaniePlikami/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Podaj ścieżkę do pliku: ");
+string path = @Console.ReadLine();
+Console.WriteLine(path);
+
+if (File.Exists(path))
+{
+    StreamReader streamReader = new StreamReader(path);
+    string data = streamReader.ReadToEnd();
+    streamReader.Close();
+    Console.WriteLine(data);
+} else
+{
+    Console.WriteLine("Plik nie istnieje, podaj treść nowego pliku. END;; aby zakończyć.");
+    File.Create(path).Close();
+    string input = " ";
+    while (!(input == "END;;")) {
+        File.AppendAllText(path, $"{input} \n");
+        input = Console.ReadLine();
+    }
+}
